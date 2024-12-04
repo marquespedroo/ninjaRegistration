4c6e2231853b627eeb5670dbebd677dbbb46774f
Pedro Henrique Marques <peterhenrike@gmail.com>
2024-12-04 11:30:43 -0300

    first commit

---
+++ src/main/java/com/steadfast/NinjaRegistration/NinjaRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.steadfast.NinjaRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steadfast/NinjaRegistration/NinjaRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steadfast.NinjaRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
