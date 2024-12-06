cbabd82466e6c7d95b12cc4cbc809b1949962f29
Pedro Henrique Marques <peterhenrike@gmail.com>
2024-12-06 13:22:16 -0300

    feat: creates mission routes controller

--- src/main/java/com/steadfast/NinjaRegistration/Missions/MissionsController.java
+++ src/main/java/com/steadfast/NinjaRegistration/Missions/MissionsController.java
@@ -1,4 +1,27 @@
 package com.steadfast.NinjaRegistration.Missions;
 
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missions")
+
 public class MissionsController {
+
+
+    @GetMapping("listMissions")
+    public String listMissions() {
+        return "These are the missions";
+    }
+
+    @PostMapping("/add")
+    public String addMission() {
+        return "Mission added.";
+    }
+
+    @PutMapping("alter")
+    public String alterMission() {
+        return "Mission altered";
+    }
+
 }
