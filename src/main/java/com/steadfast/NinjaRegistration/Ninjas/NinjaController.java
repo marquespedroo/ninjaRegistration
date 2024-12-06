2d562776691b4ae0790e816f43e05533a93614d6
Pedro Henrique Marques <peterhenrike@gmail.com>
2024-12-06 13:15:20 -0300

    feat: Updates Ninja  Controller with mock data

--- src/main/java/com/steadfast/NinjaRegistration/Ninjas/NinjaController.java
+++ src/main/java/com/steadfast/NinjaRegistration/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package com.steadfast.NinjaRegistration.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,4 +10,32 @@ public class NinjaController {
     public String welcome() {
         return "This is my first message";
     }
+
+    @PostMapping("/registerNinja")
+    public String register_ninja(){
+        return "Ninja registered";
+    };
+
+    @GetMapping("/showAll")
+    public String showAll() {
+        return "These are all the ninjas";
+    }
+
+    @GetMapping("/showAllById")
+    public String showAllById() {
+        return "These are all the ninjas ids";
+    }
+
+    @PutMapping("/alterId")
+    public String alterId() {
+        return "Alter Ninja by id";
+    }
+
+    @DeleteMapping("/deleteID")
+    public String deleteId() {
+        return "Id deleted";
+    };
+
+
+
 }
